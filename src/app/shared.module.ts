04754f1a4f1efa6d4af2b0600c730eaa2d971b34
killbond <killbond@mail.ru>
2020-08-17 02:54:56 +0300

    implemented history page

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { TrackByPropertyPipe } from "./pipes/strack-by-property.pipe";
 import { PricePipe } from "./pipes/price.pipe";
+import { DatetimePipe } from './pipes/datetime.pipe';
 
 @NgModule({
   declarations: [
     TrackByPropertyPipe,
     PricePipe,
+    DatetimePipe,
   ],
   exports: [
     TrackByPropertyPipe,
     PricePipe,
+    DatetimePipe,
   ],
   providers: [
     CurrencyPipe,
